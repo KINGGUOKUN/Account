555520e630ba19743cf0bdb3f605357c0369a2ae
KINGGUOKUN <184494760@qq.com>
2016-11-06 16:01:23 +0800

    添加日志记录组件

---
+++ AccountWeb/Account.Common/Log.cs
@@ -0,0 +1,14 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.Common
+{
+    public class Log
+    {
+        public static readonly ILog Logger = LogManager.GetLogger("Account");
+    }
+}
