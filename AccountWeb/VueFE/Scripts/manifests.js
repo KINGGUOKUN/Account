8c6e10bdf336d1e93861bd77ea79fd5f47bba290
KINGGUOKUN <184494760@qq.com>
2016-11-11 17:09:55 +0800

    添加明细

--- AccountWeb/VueFE/Scripts/manifests.js
+++ AccountWeb/VueFE/Scripts/manifests.js
@@ -1,6 +1,7 @@
 ﻿/// <reference path="index.js" />
 /// <reference path="vue.js" />
 /// <reference path="vue-resource.js" />
+/// <reference path="util.js" />
 
 const Manifests = {
     template: "#manifests",
@@ -12,7 +13,10 @@ const Manifests = {
         return {
             start: new Date(currentDate.getFullYear() - 3, 0, 1),
             end: new Date(),
-            manifests: []
+            manifests: [],
+            title:"",
+            manifest:{},
+            showOperateManifest:false
         }
     },
     methods: {
@@ -26,6 +30,28 @@ const Manifests = {
             })
                       .then(response => this.manifests = response.body)
                       .catch(response => this.$alert(response.body.Message, "日消费明细", {type:"error"}));
-        }
-    }
+        },
+        add:function(){
+            this.title = "添加消费明细";
+            this.manifest = {
+                ID:Guid.NewGuid().ToString("N"),
+                Date:new Date(),
+                Cost:"",
+                Remark:""
+            };
+            this.showOperateManifest = true;
+        },
+        save:function(){           
+            this.$http.post("http://localhost:1500/api/Manifests", this.manifest)
+            .then(() => {
+                this.manifests.push(this.manifest);
+            this.showOperateManifest = false;
+            this.$message({
+                message:"添加成功",
+                type:"success"
+            });
+        })
+            .catch(err => this.$alert(err.body.Message, "添加日消费明细", {type:"error"}));
+}
+}
 }
--- AccountWeb/VueFE/Scripts/util.js
+++ AccountWeb/VueFE/Scripts/util.js
@@ -20,3 +20,112 @@ Date.prototype.format = function (fmt) {
 
     return fmt;
 }
+
+
+//表示全局唯一标识符 (GUID)。
+function Guid(g) {
+    var arr = new Array(); //存放32位数值的数组
+    if (typeof (g) == "string") { //如果构造函数的参数为字符串
+        InitByString(arr, g);
+    }
+    else {
+        InitByOther(arr);
+    }
+    //返回一个值，该值指示 Guid 的两个实例是否表示同一个值。
+    this.Equals = function (o) {
+        if (o && o.IsGuid) {
+            return this.ToString() == o.ToString();
+        }
+        else {
+            return false;
+        }
+    }
+
+    //Guid对象的标记
+    this.IsGuid = function () { }
+
+    //返回 Guid 类的此实例值的 String 表示形式。
+    this.ToString = function (format) {
+        if (typeof (format) == "string") {
+            if (format == "N" || format == "D" || format == "B" || format == "P") {
+                return ToStringWithFormat(arr, format);
+            }
+            else {
+                return ToStringWithFormat(arr, "D");
+            }
+        }
+        else {
+            return ToStringWithFormat(arr, "D");
+        }
+    }
+    //由字符串加载
+    function InitByString(arr, g) {
+        g = g.replace(/\{|\(|\)|\}|-/g, "");
+        g = g.toLowerCase();
+        if (g.length != 32 || g.search(/[^0-9,a-f]/i) != -1) {
+            InitByOther(arr);
+        }
+        else {
+            for (var i = 0; i < g.length; i++) {
+                arr.push(g[i]);
+            }
+        }
+    }
+
+    //由其他类型加载
+    function InitByOther(arr) {
+        var i = 32;
+        while (i--) {
+            arr.push("0");
+        }
+    }
+
+    /*
+
+    根据所提供的格式说明符，返回此 Guid 实例值的 String 表示形式。
+
+    N  32 位： xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    D  由连字符分隔的 32 位数字 xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+    B  括在大括号中、由连字符分隔的 32 位数字：{xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}
+
+    P  括在圆括号中、由连字符分隔的 32 位数字：(xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)
+
+    */
+
+    function ToStringWithFormat(arr, format) {
+        switch (format) {
+            case "N":
+                return arr.toString().replace(/,/g, "");
+            case "D":
+                var str = arr.slice(0, 8) + "-" + arr.slice(8, 12) + "-" + arr.slice(12, 16) + "-" + arr.slice(16, 20) + "-" + arr.slice(20, 32);
+                str = str.replace(/,/g, "");
+                return str;
+            case "B":
+                var str = ToStringWithFormat(arr, "D");
+                str = "{" + str + "}";
+                return str;
+            case "P":
+                var str = ToStringWithFormat(arr, "D");
+                str = "(" + str + ")";
+                return str;
+            default:
+                return new Guid();
+        }
+    }
+}
+
+//Guid 类的默认实例，其值保证均为零。
+Guid.Empty = new Guid();
+
+//初始化 Guid 类的一个新实例。
+Guid.NewGuid = function () {
+    var g = "";
+    var i = 32;
+    while (i--) {
+        g += Math.floor(Math.random() * 16.0).toString(16);
+    }
+
+    return new Guid(g);
+}
