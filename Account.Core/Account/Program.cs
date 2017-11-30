bcae5f8e2c59b7f31fdf126776ea47ec6a86bca8
guokun <184494760@qq.com>
2017-11-30 21:58:09 +0800

    applicationinsights问题解决

--- Account.Core/Account/Program.cs
+++ Account.Core/Account/Program.cs
@@ -17,6 +17,7 @@ namespace Account
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseApplicationInsights()
             .UseStartup<Startup>()
             .Build();
     }
