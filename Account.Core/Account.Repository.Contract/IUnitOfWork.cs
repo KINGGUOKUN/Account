0355887e8371862e2cdc726782fb73ba1325c1d1
KINGGUOKUN <184494760@qq.com>
2018-03-17 20:14:19 +0800

    增加UnityOfWork

---
+++ Account.Core/Account.Repository.Contract/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace Account.Repository.Contract
+{
+    public interface IUnitOfWork
+    {
+        DbTransaction BeginTransaction();
+
+        void CommitTransaction();
+
+        void RollbackTransaction();
+    }
+}
---
+++ Account.Core/Account.Repository.EF/EFUnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using Account.Repository.Contract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace Account.Repository.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public EFUnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+        public DbTransaction BeginTransaction()
+        {
+            var transaction = _context.Database.GetDbConnection().BeginTransaction();
+            _context.Database.UseTransaction(transaction);
+
+            return transaction;
+        }
+
+        public void CommitTransaction()
+        {
+            _context.Database.CommitTransaction();
+        }
+
+        public void RollbackTransaction()
+        {
+            _context.Database.RollbackTransaction();
+        }
+    }
+}
