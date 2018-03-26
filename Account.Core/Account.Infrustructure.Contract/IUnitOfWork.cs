016417afe381e648d1be2a305482ffc86c2ab605
KINGGUOKUN <184494760@qq.com>
2018-03-26 21:34:03 +0800

    基础工程添加

---
+++ Account.Core/Account.Infrustructure.Contract/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Account.Infrustructure.Contract
+{
+    public interface IUnitOfWork
+    {
+        DbTransaction BeginTransaction();
+
+        void CommitTransaction();
+
+        void RollbackTransaction();
+    }
+}
---
+++ Account.Core/Account.Infrusture.EF/EFUnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using Account.Infrustructure.Contract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Account.Infrusture.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public EFUnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+        public DbTransaction BeginTransaction()
+        {
+            _context.Database.BeginTransaction();
+
+            return _context.Database.CurrentTransaction.GetDbTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            _context.SaveChanges();
+            _context.Database.CommitTransaction();
+        }
+
+        public void RollbackTransaction()
+        {
+            _context.Database.RollbackTransaction();
+        }
+    }
+}
